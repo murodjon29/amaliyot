bf91952bac13cb418899800f41b82b37b51147e8
murodjon29 <smurodjon983@gmail.com>
2025-08-21 14:29:02 +0500

    updated

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import * as express from 'express';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors())
 
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 
