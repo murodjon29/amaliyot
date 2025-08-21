04120b377bd9a658cf82dcac0432b290df8ab21c
murodjon29 <smurodjon983@gmail.com>
2025-08-21 22:40:12 +0500

    updated

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,24 +1,22 @@
-import { IsNotEmpty, IsPositive, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString, Min } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class CreateProductDto {
   @IsString()
-  @IsNotEmpty()
   name: string;
 
-  @Type(() => Number)  // string → number
-  @IsPositive()
+  @Type(() => Number)
+  @IsNumber()
+  @Min(1)
   price: number;
 
   @IsString()
-  @IsNotEmpty()
-  description: string;
+  @IsOptional()
+  description?: string;
 
-  @Type(() => Number)  // string → number
-  @IsPositive()
-  quantity: number;
-
-  // Agar form-data bilan fayl kelsa validatsiya uchun
-  // Multer `file`ni DTO ichida `Express.Multer.File` tipida olish kerak
-  file: Express.Multer.File;
+  @Type(() => Number)
+  @IsNumber()
+  @Min(1)
+  @IsOptional()
+  quantity: number = 1;
 }
